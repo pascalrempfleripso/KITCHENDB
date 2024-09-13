7923e04c365c53b91033c3077b55214a74b7a7f1
Pascal <pascal.rempfler@student.ipso.ch>
2024-09-13 15:48:46 +0200

    'db-troubleshoot'

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,11 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-#from app import routes, models
 
-@app.route('/')
-def index():
-    return jsonify({"CHOOO CHOO": "KITCHEN DB"})
+from app import routes, models
--- app/models.py
+++ app/models.py
@@ -1,11 +1,14 @@
-from datetime import datetime
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
 from app import db
 
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return "<User {}>".format(self.username)
