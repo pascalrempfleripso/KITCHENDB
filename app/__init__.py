b4d75726438f367e8c74b071f1a18e3d7ca560fb
Pascal <pascal.rempfler@student.ipso.ch>
2024-09-13 16:40:49 +0200

    sql added in init

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
-from config import Config
-#from flask_sqlalchemy import SQLAlchemy
-#from flask_migrate import Migrate
-
+# from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config.from_object(Config)
-#db = SQLAlchemy(app)
-#migrate = Migrate(app, db)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+py{MYSQL_URL}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable to avoid warnings
+#app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 from app import routes
