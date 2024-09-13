bb2fb3cca9ec38af64453a3beaea3e47bc8b53ae
Pascal <pascal.rempfler@student.ipso.ch>
2024-09-13 16:49:02 +0200

    sql secret key added

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask
-# from config import Config
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+py{MYSQL_URL}'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable to avoid warnings
-#app.config.from_object(Config)
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
+
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-#    SQLALCHEMY_DATABASE_URI = os.environ.get('MYSQL_URL')
-#    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+    SQLALCHEMY_DATABASE_URI = "mysql+py" + os.environ.get("MYSQL_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
