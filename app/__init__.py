2cbe3b9d656eb5485775cabbace195799f55a03e
Pascal <pascal.rempfler@student.ipso.ch>
2024-09-13 15:12:50 +0200

    'login-mask-added-and-init'

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask, jsonify
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from app import routes, models
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+from app import routes, models
 
 @app.route('/')
 def index():
