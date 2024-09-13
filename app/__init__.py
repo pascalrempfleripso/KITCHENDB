d29be6ca1e885384a97a87d940880fa00084a8d5
Pascal <pascal.rempfler@student.ipso.ch>
2024-09-13 14:48:42 +0200

    'test'

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    return jsonify({"CHOOO CHOO": "KITCHEN DB"})
