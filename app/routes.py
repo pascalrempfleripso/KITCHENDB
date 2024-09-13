c3f12e810a49f2774358c19041497dad7ecb0729
Pascal <pascal.rempfler@student.ipso.ch>
2024-09-13 15:08:00 +0200

    'login-mask-added'

---
+++ app/routes.py
@@ -0,0 +1,29 @@
+# app/routes.py
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+  user = {'username': 'Pascal'}
+  posts = [
+     {
+        'author': {'username': 'Pascal'},
+        'body': 'Offene Calzone'
+     }
+  ]
+  return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+
+
+
+#Login Form:
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+   form = LoginForm()
+   if form.validate_on_submit():
+      flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+      return redirect(url_for('index'))
+   return render_template('login.html', title='Sign In', form=form)
