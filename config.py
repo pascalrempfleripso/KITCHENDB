dfd831108060993551a0e2859e3719463cb7c843
Pascal <pascal.rempfler@student.ipso.ch>
2024-09-13 15:58:25 +0200

    added mysql driver

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('MYSQL_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
